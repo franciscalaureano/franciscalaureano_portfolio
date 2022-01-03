26306c8e25f7cd0e686d2fcb3c88958304c7c107
franciscalaureano <76536458+franciscalaureano@users.noreply.github.com>
2022-01-03 00:45:32 +0000

    update media query

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-const mediaQueryIpad = window.matchMedia('(max-width: 1076px)')
+const mediaQueryIpad = window.matchMedia('(max-width: 1100px)')
 const mediaQueryPc = window.matchMedia('(max-width: 1512px)')  
   var htmlCollection = document.getElementsByClassName('slide_item');
   //getting elements by class name into an HTMLCollection
