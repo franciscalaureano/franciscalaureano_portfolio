6f2a648c29a96f9bfa88cb4615dd027be98045fc
franciscalaureano <76536458+franciscalaureano@users.noreply.github.com>
2022-01-07 12:55:05 +0000

    media query pc l

--- js/media_query.js
+++ js/media_query.js
@@ -1,4 +1,4 @@
 const mediaQueryMobile = window.matchMedia('(max-width: 500px)');
 const mediaQueryIpad = window.matchMedia('(max-width: 1100px)');
 const mediaQueryPcM = window.matchMedia('(max-width: 1512px)') ;
-const mediaQueryPcL = window.matchMedia('(max-width: 2500px)') 
+const mediaQueryPcL = window.matchMedia('(max-width: 3000px)') 
